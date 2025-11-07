c409dcd09b30b7788a51c970345508024ac418d5
David Czeller <davidczeller@MacBookPro.home>
2025-11-07 18:19:00 +0100

    Simplify: Back to standard NestJS setup, Vercel will run as Node app

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,9 @@ async function bootstrap() {
     new ValidationPipe({
       transform: true,
       whitelist: true,
-      // forbidNonWhitelisted: true, production mode
-      // disableErrorMessages: true, production mode
     }),
   );
   await app.listen(process.env.PORT ?? 3000);
 }
-void bootstrap();
+
+bootstrap();
