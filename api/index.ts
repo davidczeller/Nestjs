a051df6cdaa1e3f0ce5804e255fc75367051a42e
David Czeller <davidczeller@MacBookPro.home>
2025-11-07 17:56:17 +0100

    Fix: Configure NestJS for Vercel serverless deployment

---
+++ api/index.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from '../src/app.module';
+import { ValidationPipe } from '@nestjs/common';
+import express = require('express');
+
+const server = express();
+
+const createNestServer = async (expressInstance: express.Application) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
+  return app.init();
+};
+
+createNestServer(server)
+  .then(() => console.log('Nest Ready'))
+  .catch((err) => console.error('Nest broken', err));
+
+export default server;
